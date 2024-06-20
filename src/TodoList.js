53e9366fb50eb6e8f5e5e1f0de2b83eb4c68cf17
CarolinaRojas <carolirojas73@gmail.com>
2024-06-20 17:18:47 -0400

    Primer React con JSX

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+//CAROLINA ROJAS COLLANTE
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+  );
+}
+
+export default TodoList;
